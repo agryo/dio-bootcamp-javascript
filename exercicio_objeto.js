a2c8d2079487382f552217ba78dc37747293c688
Agryo Araujo <agryostallion@gmail.com>
2023-04-29 22:14:46 -0300

    Exercicio de objeto

---
+++ exercicio_objeto.js
@@ -0,0 +1,33 @@
+/*1) Crie uma classe para representar carros. 
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+gasto em reais para realizar este percurso.
+*/
+class Carros {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor (marca, cor, gastoMedioPorKm){
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+}
+
+function calcularValorGasto(gastoMedioPorKm, preco){
+    return preco / gastoMedioPorKm;
+}
+
+const uno = new Carros('Fiat', 'Preto', 12);
+
+console.log('R$' + calcularValorGasto(uno.gastoMedioPorKm, 5.89).toFixed(2) + ' por Km.');
+
+
+/*
+2) Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;
+*/
