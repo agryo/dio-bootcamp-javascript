bafe28ee210117573f11b7e69bbacb17c23a1e7d
Agryo Araujo <agryostallion@gmail.com>
2023-05-05 23:43:36 -0300

    Avaliação 2 concluída

---
+++ 08 - Avaliação/avaliacao2.js
@@ -0,0 +1,41 @@
+/*
+2) Faça um programa que receba N quantidade de números e seus respectivos valores.
+Imprima o maior número par e o menor número impar.
+
+    Exemplo:
+        Entrada:
+            5
+            3
+            4
+            1
+            10
+            8
+
+        Saída:
+            Maior número par: 10
+            Menor número impar: 1
+*/
+function maiorPar(numeros) {
+    maiorPar = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (maiorPar < numeros[i]) {
+            maiorPar = numeros[i];
+        }        
+    }
+    return maiorPar;
+}
+
+function menorImpar(numeros) {
+    menorImpar = 99;
+    for (let i = 0; i < numeros.length; i++) {
+        if (menorImpar > numeros[i]) {
+            menorImpar = numeros[i];
+        }        
+    }
+    return menorImpar;
+}
+
+const numeros = [5, 3, 4, 1, 10, 8];
+
+console.log('Maior número Par: ' + maiorPar(numeros));
+console.log('Menor número Impar: ' + menorImpar(numeros));
